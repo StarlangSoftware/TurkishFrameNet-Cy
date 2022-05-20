ef72352dc00938b94d7c2513c19da8055e86b06a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-20 10:51:33 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["FrameNet/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-FrameNet-Cy',
-    version='1.0.6',
+    version='1.0.7',
     packages=['FrameNet', 'FrameNet.data'],
     package_data={'FrameNet': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'FrameNet.data': ['*.xml']},
