3ab6968eddd682d3dc7d704599bb086ebab104d6
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-05-02 19:24:33 +0300

    Updated documentation.

--- FrameNet/FrameNet.pyx
+++ FrameNet/FrameNet.pyx
@@ -5,6 +5,10 @@ import pkg_resources
 cdef class FrameNet:
 
     def __init__(self):
+        """
+        A constructor of FrameNet class which reads all frame files inside the files2.txt file. For each filename inside
+        that file, the constructor creates a FrameNet.Frame and puts in inside the frames array.
+        """
         self.__frames = []
         root = xml.etree.ElementTree.parse(pkg_resources.resource_filename(__name__, 'data/framenet.xml')).getroot()
         for frame_node in root:
@@ -19,6 +23,11 @@ cdef class FrameNet:
             self.__frames.append(frame)
 
     cpdef bint lexicalUnitExists(self, str synSetId):
+        """
+        Checks if the given lexical unit exists in any frame in the frame set.
+        :param synSetId: Id of the lexical unit
+        :return: True if any frame contains the given lexical unit, false otherwise.
+        """
         cdef Frame frame
         for frame in self.__frames:
             if frame.lexicalUnitExists(synSetId):
@@ -26,6 +35,11 @@ cdef class FrameNet:
         return False
 
     cpdef list getFrames(self, str synSetId):
+        """
+        Returns an array of frames that contain the given lexical unit in their lexical units
+        :param synSetId: Id of the lexical unit.
+        :return: An array of frames that contains the given lexical unit.
+        """
         cdef list result
         cdef Frame frame
         result = []
@@ -35,7 +49,16 @@ cdef class FrameNet:
         return result
 
     cpdef int size(self):
+        """
+        Returns number of frames in the frame set.
+        :return: Number of frames in the frame set.
+        """
         return len(self.__frames)
 
     cpdef Frame getFrame(self, int index):
+        """
+        Returns the element at the specified position in the frame list.
+        :param index: index of the element to return
+        :return: The element at the specified position in the frame list.
+        """
         return self.__frames[index]
